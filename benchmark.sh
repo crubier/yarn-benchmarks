97ec941e1a88769321be173246a79535035591c0
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2020-09-09 14:23:27 +0100

    Add yarn node_modules

--- benchmark.sh
+++ benchmark.sh
@@ -1,48 +1,63 @@
 #!/usr/bin/env bash
 
-echo "========================================================================================"
+# echo "========================================================================================"
 
 cd single-berry
 echo "berry"
 echo ""
-time (yarn install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
-echo ""
+# time (yarn install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
+# echo ""
 time (yarn build-storybook > /dev/null 2>&1 && echo "build-storybook ok" || echo "build-storybook fail")
 echo ""
-time (yarn build > /dev/null 2>&1 && echo "build ok" || echo "build fail")
-echo ""
-time (yarn test --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail")
-echo ""
+# time (yarn build > /dev/null 2>&1 && echo "build ok" || echo "build fail")
+# echo ""
+# time (yarn test --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail")
+# echo ""
 cd ..
 
-echo "========================================================================================"
+# echo "========================================================================================"
 
-cd single-yarn
-echo "yarn"
+cd single-berry-no-pnp
+echo "berry-no-pnp"
 echo ""
-time (yarn install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
-echo ""
+# time (yarn install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
+# echo ""
 time (yarn build-storybook > /dev/null 2>&1 && echo "build-storybook ok" || echo "build-storybook fail")
 echo ""
-time (yarn build > /dev/null 2>&1 && echo "build ok" || echo "build fail")
-echo ""
-time (yarn test --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail")
-echo ""
+# time (yarn build > /dev/null 2>&1 && echo "build ok" || echo "build fail")
+# echo ""
+# time (yarn test --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail")
+# echo ""
 cd ..
 
-echo "========================================================================================"
+# echo "========================================================================================"
 
-cd single-npm
-echo "npm"
-echo ""
-time (npm install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
-echo ""
-time (npm run build-storybook > /dev/null 2>&1 && echo "build-storybook ok" || echo "build-storybook fail") 
-echo ""
-time (npm run build > /dev/null 2>&1 && echo "build ok" || echo "build fail") 
-echo ""
-time (npm run test -- --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail") 
-echo ""
-cd ..
+# cd single-yarn
+# echo "yarn"
+# echo ""
+# time (yarn install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
+# echo ""
+# time (yarn build-storybook > /dev/null 2>&1 && echo "build-storybook ok" || echo "build-storybook fail")
+# echo ""
+# time (yarn build > /dev/null 2>&1 && echo "build ok" || echo "build fail")
+# echo ""
+# time (yarn test --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail")
+# echo ""
+# cd ..
+
+# echo "========================================================================================"
+
+# cd single-npm
+# echo "npm"
+# echo ""
+# time (npm install > /dev/null 2>&1 && echo "install ok" || echo "install fail")
+# echo ""
+# time (npm run build-storybook > /dev/null 2>&1 && echo "build-storybook ok" || echo "build-storybook fail") 
+# echo ""
+# time (npm run build > /dev/null 2>&1 && echo "build ok" || echo "build fail") 
+# echo ""
+# time (npm run test -- --watchAll=false > /dev/null 2>&1 && echo "test ok" || echo "test fail") 
+# echo ""
+# cd ..
 
 echo "========================================================================================"
