c430a63c8dfa680ace0cc097eb18570c9dc4e9c3
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2020-09-10 09:26:13 +0100

    no dll

--- single-yarn-nm/.storybook/main.js
+++ single-yarn-nm/.storybook/main.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+ 
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.plugins.push(new BundleAnalyzerPlugin());
+
+    // Return the altered config
+    return config;
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
